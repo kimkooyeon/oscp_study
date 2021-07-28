66af3d8027734f01a552cdb175d3364094f88905
kimkooyeon <kimkooyeon@gmail.com>
2021-07-28 17:12:13 +0800

    Update doping.sh

--- doping.sh
+++ doping.sh
@@ -1,6 +1,8 @@
+
 #!/bin/bash
 
 for ip in `seq 1 254`;do
 #echo "ping 192.168.32.$ip"
-ping -c 1 "192.168.32.$ip"| grep "bytes from" &
+#ping -c 1 "192.168.32.$ip"| grep "bytes from" &
+ping -c 1 "192.168.32.$ip"| grep "bytes from" | cut -d " " -f4 | cut -d ":" -f1>
 done
