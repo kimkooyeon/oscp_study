e313909f7aab01706b1f6e6bf4f49a7429429daf
kimkooyeon <kimkooyeon@gmail.com>
2021-08-11 19:33:42 +0800

    Update zone_transfer.sh

--- zone_transfer.sh
+++ zone_transfer.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Check if argument was given. If not print usage
+if [ -z "$1" ]; then
+        echo "Usage:"
+        echo "$0 abc.com"
+        exit 0
+fi
+
+for server in $(host -t ns $1 | cut -d" " -f4); do
+host -l kim.sg $server;
+done
